066b02c8e6189a46837f43e6ae7a65d1cbec0257
chrisbodhi <cboette@gmail.com>
2013-12-29 11:41:11 -0600

    Add ability to sign up. Add test for sign-up pass and fail. Add styling. Add error partials.

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -13,7 +13,29 @@ describe "User pages" do
   end
 
   describe "Signup page" do
+
     before { visit signup_path }
+
+    let(:submit) { "Create my account" }
+
+    describe "with invalid information" do 
+      it "should not create a user" do 
+        expect { click_button submit }.not_to change(User, :count)
+      end
+    end
+
+    describe "with valid information" do 
+      before do 
+        fill_in "Name",                   with: "Example User"
+        fill_in "Email",                  with: "user@exa.com"
+        fill_in "Password",               with: "foobar"
+        fill_in "Password confirmation",  with: "foobar"
+      end
+
+      it "should create a user" do 
+        expect { click_button submit }.to change(User, :count).by(1)
+      end
+    end
 
     it { should have_content('Sign up') }
     it { should have_title(full_title('Sign up')) }
