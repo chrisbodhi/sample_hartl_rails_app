2b00ca6e6762da497a8fd81594f501b52350410e
chrisbodhi <cboette@gmail.com>
2014-01-01 13:31:23 -0600

    Finish user CRUD actions and respective tests.

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,12 @@
 FactoryGirl.define do 
   factory :user do 
-    name      "Mikey Hartl"
-    email     "mikey@mail.com"
+    sequence(:name)   { |n| "Person #{n}" }
+    sequence(:email)   { |n| "person_#{n}@example.com" }
     password  "foobar"
     password_confirmation  "foobar"
+    
+    factory :admin do 
+      admin true
+    end
   end
 end
